cd815c7ca1bd071bd5967596ecb2f54bf986fa07
laramell <laracosta.acad@gmail.com>
2023-11-10 03:00:02 -0300

    new

---
+++ pages/home.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Lara Mel - Design</title>
+
+        <link rel="stylesheet" href="/style/fonts.css">
+        <link rel="stylesheet" href="/style/home.css">
+    </head>
+    <body>
+        <header>
+            <a id="logo" href="/"><img src="/assets/logo.svg" alt="Lara Mel"></a>
+            <nav>
+                <ul>
+                    <li><a href="">Início</a></li>
+                    <li><a href="https://www.behance.net/laramel28">Portfólio</a></li>
+                    <li><a id="last" href="">Preciso de um projeto</a></li>
+                </ul>
+            </nav>
+        </header>
+        <section>
+            <h1>
+                Design que <br>
+                cativa, comunica <br>
+                & transforma.
+            </h1>
+            <h2>
+                Histórias de marcas ganham <br>
+                vida com uma identidade visual <br>
+                autêntica e memorável
+            </h2>
+            <span id="back-h2"></span>
+        </section>
+    </body>
+</html>
